5d43e57e684ed79717649d27342fcce16da2d2fa
you@example.com <you@example.com>
2025-10-30 17:36:02 +0100

    bolas

--- Practica/app.js
+++ Practica/app.js
@@ -1,9 +1,31 @@
-const http = requiere('http');
+"use strict";
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 //Establecimiento de la función callback del servidor
 let servidor = http.createServer(function (request, response) {
     // funcionalidad del servidor
-    console.log('Método: ${request.method}');
-    console.log('URL: ${request.url}');
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+    const rutaPublica = path.join(__dirname, 'public');  // Carpeta donde están los archivos estáticos
+
+    // Si la ruta es la raíz, enviar el archivo index.html
+    if (request.url === '/') {
+        const archivoHTML = path.join(__dirname, 'public', 'html', 'index_es.html');
+        fs.readFile(archivoHTML, 'utf-8', (err, data) => {
+            if (err) {
+                response.statusCode = 500;
+                response.end('Error al cargar el archivo HTML');
+            } else {
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/html');
+                response.end(data);
+            }
+        });
+    }
+
+    console.log(`Método: ${request.method}`);
+    console.log(`URL: ${request.url}`);
     console.log(request.headers);
 });
 // Inicio del servidor
@@ -11,5 +33,5 @@ servidor.listen(3000, function (err) {
     if (err)
         console.log("Error al iniciar el servidor");
     else
-        console.log("Servidor escuchando en el puerto 3000");
+        console.log("Servidor escuchando en el puerto 3000: http://localhost:3000/");
 });
