e45c1eadf203b449fa328df78183465f5efacd31
luispe06 <luispe06@ucm.es>
2025-11-17 20:41:26 +0100

    uy

--- Practica/app.js
+++ Practica/app.js
@@ -2,17 +2,48 @@
 
 const express = require('express');
 const path = require('path');
-const { cargarDatosIniciales } = require('./src/config/dataLoader');
+const morgan = require('morgan');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const { cargarDatosIniciales } = require('./config/dataLoader');
+
+const mainRoutes = require('./routes/mainRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const empleadoRoutes = require('./routes/empleadoRoutes');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "public", "views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'tu_clave_secreta_aqui',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use('/', mainRoutes);
+app.use('/admin', adminRoutes);
+app.use('/empleado', empleadoRoutes);
+
+app.use((req, res, next) => {
+    res.status(404);
+    res.render("error404", { url: req.url });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500);
+    res.render("error500", { 
+        mensaje: err.message, 
+        pila: err.stack 
+    });
 });
 
 app.listen(PORT, () => {
